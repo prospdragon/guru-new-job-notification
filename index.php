15616ee2d61949235146b5ae8c9aad6781e6cce0
Jorge <super8_dragon@163.com>
2021-10-09 03:48:07 +0800

    Add favicon

--- index.php
+++ index.php
@@ -2,6 +2,13 @@
 <html lang="en">
 <head>
     <title>New Job Checker</title>
+
+	<link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png" />
+	<link rel="icon" type="image/png" href="favicon/favicon-32x32.png" sizes="32x32" />
+	<link rel="icon" type="image/png" href="favicon/favicon-16x16.png" sizes="16x16" />
+	<link rel="mask-icon" href="favicon/safari-pinned-tab.svg" color="#4c83c3" />
+	<link rel="shortcut icon" href="favicon/favicon.ico" />
+
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
